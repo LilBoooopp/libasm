33c5e1d0121a6dc8fb86bc0dcd880142d982817f
LilBoooopp <cbopp@student.42lausanne.ch>
2026-02-14 18:22:37 +0100

    strcpy

---
+++ ft_strcpy.s
@@ -0,0 +1,23 @@
+global  ft_strcpy
+section .text
+
+	; ft_strcpy(char *dst, const char *src)
+	; rdi = dst
+	; rsi = src
+
+ft_strcpy:
+	xor rax, rdi
+
+.loop:
+	mov dl, byte [rsi]
+	mov byte [rdi], dl
+
+	cmp dl, 0
+	je  .done
+
+	inc rdi
+	inc rsi
+	jmp .loop
+
+.done:
+	ret
