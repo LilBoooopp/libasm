a022214d4c5198c40898b681690e0314e04cf463
LilBoooopp <cbopp@student.42lausanne.ch>
2026-02-16 20:48:30 +0100

    write

---
+++ ft_write.s
@@ -0,0 +1,28 @@
+global  ft_write
+section .text
+extern  __errno_location
+
+	;ssize_t write(int fd, const void buf[.count], size_t count)
+	; rdi = fd
+	; rsi = *buf
+	; rsx = count
+
+ft_write:
+	mov rax, 1
+	syscall
+
+	cmp rax, 0
+	jge .ok
+
+	push rbx
+	mov  rbx, rax
+	call __errno_location
+	neg  rbx
+	mov  dword [rax], ebx
+	pop  rbx
+
+	mov rax, -1
+	ret
+
+.ok:
+	ret
