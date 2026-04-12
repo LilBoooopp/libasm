f973b886b1c1552bc1e6d2ed67135ccb82b81b8a
LilBoooopp <cbopp@student.42lausanne.ch>
2026-04-12 23:12:22 +0200

    comments

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -6,16 +6,23 @@ extern malloc
 	; rsi = *data
 
 ft_list_push_front:
+	; save registers and push rdx for stack alignment
 	push rdi
 	push rsi
 	push rdx
+
+	; malloc 16 bytes for a t_list node
 	mov  rdi, 16
 	call malloc wrt ..plt
+
+	; check null
 	test rax, rax
 	pop  rdx
 	pop  rsi
 	pop  rdi
+	; if malloc failed then return
 	jz   .ret
+
 	mov  [rax], rsi
 	mov  rsi, [rdi]
 	mov  [rax+8], rsi
